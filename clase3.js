bfbae51b0cbe86dc84b29c0f7462eaed73077949
valeria <vidallopezvaleria375@gmail.com>
2024-10-30 19:15:29 -0500

    subiendo apuntes clase 3

---
+++ clase3.js
@@ -0,0 +1,16 @@
+//crear objeto
+
+const usuario ={
+    nombre: 'Valeria',
+    edad: 20,
+    trabaja: true,
+}
+//imprimir a las propiedades de un objeto 
+
+console.log(usuario)
+
+//imprimir una propiedad en especifico de un objeto
+console.log(usuario.nombre)
+console.log(usuario.edad)
+console.log(usuario.trabaja)
+
