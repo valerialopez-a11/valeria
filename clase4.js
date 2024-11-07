29be3a64e07f116067848770c39672c3dcf7567e
valeria <vidallopezvaleria375@gmail.com>
2024-11-06 19:54:49 -0500

    clase goku

--- clase4.js
+++ clase4.js
@@ -3,10 +3,29 @@ class Goku{
         this.nombre = nombre;
         this.nivel = nivel; //nivel actual de la transformacion 
     }
-}
-class Goku {
     mostrarTransformacion(){
         return `${this.nombre} esta en la transformacion:
         ${this.nivel}`
     }
+    transformar(nuevaTransformacion){
+        this.nivel = nuevaTransformacion;
+        return `${this.nombre} se ha transformado en ${this.nivel}!`;
+    }
+    volverFormaBase(){
+        this.nivel = 'Base'
+        return `${this.nombre} ha vuelto a su forma base.`
+    }
+
 }
+//creamos un objeto Goku con su forma base
+let goku = new Goku('Goku', 'Base');
+console.log(goku)
+
+//mostrar la transformacion actual
+console.log(goku.mostrarTransformacion())
+
+//transformar a super saiyan
+console.log(goku.transformar('Super Saiyan Dios'))
+
+//volver a la forma base
+console.log(goku.volverFormaBase())
